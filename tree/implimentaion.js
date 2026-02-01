698e7f6bee36673bc6fe42380e77bd853669242e
debashismidya <debashis.matainja@gmail.com>
2026-02-02 00:25:41 +0530

    tree 1

---
+++ tree/implimentaion.js
@@ -0,0 +1,50 @@
+class Node{
+    constructor(value){
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root=null;
+    }
+    insert(value){
+        const newNode = new Node(value);
+        if (this.root===null) {
+            this.root=newNode
+        }else{
+            let currentNode = this.root;
+            while (true) {
+                if (value<currentNode.value) {
+                    //left 
+                    if (!currentNode.left) {
+                        currentNode.left=newNode;
+                        return this;
+                    }
+                    currentNode=currentNode.left
+                }else{
+                    if (!currentNode.right) {
+                        currentNode.right=newNode;
+                        return this;
+                    }
+
+                    currentNode = currentNode.right
+                }
+            }
+        }
+
+    }
+}
+const tree= new BinarySearchTree()
+tree.insert(9);
+tree.insert(4);
+tree.insert(20);
+tree.insert(1);
+tree.insert(6);
+tree.insert(170);
+tree.insert(15);
+
+
+console.log(JSON.stringify(tree.root));
